62e590609aa4a8737bf4953c3fd6f55e7cdfca01
Vivian-Tung <viviantung@live.ca>
2023-03-05 14:26:04 -0800

    add circle

--- popup.js
+++ popup.js
@@ -5,10 +5,15 @@ const tim = {
     longBreakInterval: 4,
   };
 
+const TIME_LIMIT = 20;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+
 document.addEventListener('DOMContentLoaded', function(){
     var link = document.getElementById("default");
     link.addEventListener('click', function(){
-        startTimer(tim.pomodoro,0);
+        printStartTimer(tim.pomodoro,0);
         //console.log('Hello');
     })
 })
@@ -16,19 +21,26 @@ document.addEventListener('DOMContentLoaded', function(){
 document.addEventListener('DOMContentLoaded', function(){
     var link = document.getElementById("short");
     link.addEventListener('click', function(){
-        startTimer(tim.shortBreak,0);
+        printStartTimer(tim.shortBreak,0);
     })
 })
 
 document.addEventListener('DOMContentLoaded', function(){
     var link = document.getElementById("long");
     link.addEventListener('click', function(){
-        startTimer(tim.longBreak,0);
+        printStartTimer(tim.longBreak,0);
     })
 })
 
+//start time when press on start 
+document.addEventListener('DOMContentLoaded', function(){
+    var link = document.getElementById("start");
+    link.addEventListener('click', function() {
+        startTimer();
+    })
+})
 
-function startTimer(min, sec){
+function printStartTimer(min, sec){
     if (sec < 10){
         sec = "0" + String(sec)
     }
@@ -36,10 +48,35 @@ function startTimer(min, sec){
     document.getElementById("timer").innerHTML = tim2;
 }
 
-function startDecreasing(){
-    
-}
 
-// function test1() {
-//     console.log("Here");
+
+// function startTimer(){
+//     timerInterval = setInterval(() => {
+//         //amount of time passed increments by one 
+//         timePassed = timePassed += 1;
+//         timeLeft = TIME_LIMIT - timePassed;
+
+//         //time left label is updated 
+//         document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
+
+//     }, 1000);
 // }
+
+
+
+
+function formatTimeLeft(tim) {
+    //largest round integer less than or equal to the result of the time being divded by 60
+    const minutes = Math.floor(tim/60);
+
+    //seconds reminder of time division
+    let seconds = tim % 60;
+
+    // If the value of seconds is less than 10, then display seconds with a leading zero
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+
+    // The output in MM:SS format
+    return `${minutes}:${seconds}`;
+}
